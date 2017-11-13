37ed1f5900d9aa89fb5fdb5f02868c8e13c582db
Dan D'Avella <drdavella@gmail.com>
2017-11-12 21:34:39 -0500

    Create simple app with command line parser

--- src/main.ml
+++ src/main.ml
@@ -1,1 +1,18 @@
-print_string "Hello world!\n";;
+open Core.Std
+
+let run filename =
+  printf "rom file=%s\n" filename
+
+let spec =
+  let open Command.Spec in
+  empty
+  +> anon ("rom file" %: string)
+
+let command =
+  Command.basic
+    ~summary:"Run the GBC emulator"
+    spec
+    (fun filename () -> run filename)
+
+let () =
+  Command.run ~version:"1.0" ~build_info:"RWO" command
