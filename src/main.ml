245d61b8d06647ebe169218e51131a4674fad342
Dan D'Avella <drdavella@gmail.com>
2017-11-27 21:35:23 -0500

    Read rom file into array

--- src/main.ml
+++ src/main.ml
@@ -1,7 +1,25 @@
 open Core.Std
+open Core_kernel.In_channel
+
+
+let rec process_rom infile =
+  let byte = match input_byte infile with
+  | Some x -> [x] :: process_rom infile
+  | None -> []
+  in
+  byte
+
+let read_rom filename =
+  let rom_list = In_channel.with_file filename ~f:process_rom in
+  Array.of_list rom_list
+
+let emulate rom =
+  printf "running program\n"
 
 let run filename =
-  printf "rom file=%s\n" filename
+  let rom_array = read_rom filename in
+  let () = printf "rom file=%s, size=%d\n" filename (Array.length rom_array) in
+  emulate rom_array
 
 let spec =
   let open Command.Spec in
