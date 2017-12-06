9ffd29bc15801f8c2477d29e10993d6d779a5082
Dan D'Avella <drdavella@gmail.com>
2017-12-05 21:20:48 -0500

    First pass at emulation loop (imperative)

--- src/main.ml
+++ src/main.ml
@@ -4,7 +4,7 @@ open Core_kernel.In_channel
 
 let rec process_rom infile =
   let byte = match input_byte infile with
-  | Some x -> [x] :: process_rom infile
+  | Some x -> x :: process_rom infile
   | None -> []
   in
   byte
@@ -13,8 +13,11 @@ let read_rom filename =
   let rom_list = In_channel.with_file filename ~f:process_rom in
   Array.of_list rom_list
 
-let emulate rom =
-  printf "running program\n"
+let emulate rom_array =
+  let length = Array.length rom_array in
+  for i = 0 to length - 1 do
+    printf "rom[%d]=0x%x\n" i rom_array.(i)
+  done
 
 let run filename =
   let rom_array = read_rom filename in
