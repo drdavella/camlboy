7cd039b168718b91fe92353562457259e9bc52f9
Dan D'Avella <drdavella@gmail.com>
2017-12-06 21:38:42 -0500

    Incremental changes. Simplify process_rom function

--- src/main.ml
+++ src/main.ml
@@ -3,26 +3,22 @@ open Core_kernel.In_channel
 
 
 let rec process_rom infile =
-  let byte = match input_byte infile with
+  match input_byte infile with
   | Some x -> x :: process_rom infile
   | None -> []
-  in
-  byte
 
 let read_rom filename =
   let rom_list = In_channel.with_file filename ~f:process_rom in
   Array.of_list rom_list
 
-let emulate rom_array =
-  let length = Array.length rom_array in
-  for i = 0 to length - 1 do
-    printf "rom[%d]=0x%x\n" i rom_array.(i)
-  done
+let rec emulate rom_array index =
+  printf "rom[%04x]=%02x\n" index rom_array.(index);
+  emulate rom_array (index + 1)
 
 let run filename =
   let rom_array = read_rom filename in
-  let () = printf "rom file=%s, size=%d\n" filename (Array.length rom_array) in
-  emulate rom_array
+  printf "rom file=%s, size=%d\n" filename (Array.length rom_array);
+  emulate rom_array 0
 
 let spec =
   let open Command.Spec in
