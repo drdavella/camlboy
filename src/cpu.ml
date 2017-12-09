a2041a04db61f9c13a82cf0a5a77f769bdfc82f6
Daniel D'Avella <drdavella@gmail.com>
2017-12-09 10:58:45 -0500

    Updates to use ppx_bitstring and OCaml 4.04.0

--- src/cpu.ml
+++ src/cpu.ml
@@ -1,7 +1,13 @@
-open Core.Std
+open Core
+
+let decode byte =
+  let%bitstring bits = {|byte : 8|} in
+  match%bitstring bits with
+  | {| 0xff : 8 |} -> printf "it's an 0xff!\n"
+  | {| _ |} -> printf "unknown code byte=0x%02x\n" byte
 
 let rec emulate rom_array index rom_size =
   if index < rom_size then
     let byte = rom_array.(index) in
-    printf "rom[%04x]=%02x\n" index byte;
+    decode byte;
     emulate rom_array (index + 1) rom_size
