c49c266100393a54cd4dc8b052569a51d5b60fa5
Daniel D'Avella <drdavella@gmail.com>
2017-12-21 18:25:37 -0500

    Intermediate commit: working on named register implementation

--- src/memory.ml
+++ src/memory.ml
@@ -1,6 +1,14 @@
 open Core
 open Types
 
-let load_register dest source code_bytes state =
-      let msg = sprintf "HEY GUYS! 0x%1x 0x%1x\n" dest source in
-      (Utils.increment_pc state 1), 4, msg
+let source_dest_str dest source =
+  sprintf "%s <- %s" (register_to_str dest) (register_to_str source)
+
+let dest_index high low =
+  (high / low) * (high - 0x4)
+
+let load_register high low code_bytes state =
+  let source_reg = register_array.(low) in
+  let dest_reg = register_array.(dest_index high low) in
+  let msg = (sprintf "LD ") ^ (source_dest_str dest_reg source_reg) in
+  (Utils.increment_pc state 1), 4, msg
--- src/types.ml
+++ src/types.ml
@@ -1,12 +1,47 @@
+open Core
 open Unsigned
 
 let rom_start_index = UInt16.of_int 0x100
+
+type real_register =
+  | A | B | C | D | E | H | L
+
+type named_register =
+  | Reg of real_register
+  | HL
+
+let register_array =
+  Array.of_list [ Reg B ; Reg C ; Reg D ; Reg E ; Reg H ; Reg L ; HL; Reg A ]
+
+let register_to_str reg =
+  match reg with
+  | Reg A -> "A"
+  | Reg B -> "B"
+  | Reg C -> "C"
+  | Reg D -> "D"
+  | Reg E -> "E"
+  | Reg H -> "H"
+  | Reg L -> "L"
+  | HL -> "HL"
+
+type registers =
+  {
+    mutable a  : uint16;
+    mutable b  : uint16;
+    mutable c  : uint16;
+    mutable d  : uint16;
+    mutable e  : uint16;
+    mutable h  : uint16;
+    mutable l  : uint16;
+  }
 
 type game_state =
   {
     mutable pc : uint16;
     mutable flags : uint8;
     mutable ticks : int;
+    memory : UInt16.t Array.t;
+    registers : registers;
   }
 
 let init_game_state =
@@ -14,4 +49,15 @@ let init_game_state =
     pc = rom_start_index;
     flags = UInt8.of_int 0;
     ticks = 0;
+    memory = Array.create ~len:100 (UInt16.of_int 0);
+    registers =
+    {
+      a = UInt16.of_int 0;
+      b = UInt16.of_int 0;
+      c = UInt16.of_int 0;
+      d = UInt16.of_int 0;
+      e = UInt16.of_int 0;
+      h = UInt16.of_int 0;
+      l = UInt16.of_int 0;
+    }
   }
